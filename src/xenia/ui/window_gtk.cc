d146e3192106295cdef6414d31ab274d57c2571d
Sandy Carter <bwrsandman@gmail.com>
2019-01-18 09:01:11 -0500

    [vulkan linux] Fix native_platform_handle with gtk
    Use gtk connection as handle and use it when creating surface.

--- src/xenia/ui/window_gtk.cc
+++ src/xenia/ui/window_gtk.cc
@@ -81,6 +81,10 @@ void GTKWindow::Create() {
                    NULL);
   g_signal_connect(G_OBJECT(window_), "event", G_CALLBACK(gtk_event_handler_),
                    reinterpret_cast<gpointer>(this));
+
+  GdkDisplay* gdk_display = gtk_widget_get_display(window_);
+  assert(GDK_IS_X11_DISPLAY(gdk_display));
+  connection_ = XGetXCBConnection(gdk_x11_display_get_xdisplay(gdk_display));
 }
 
 bool GTKWindow::OnCreate() {
--- src/xenia/ui/window_gtk.h
+++ src/xenia/ui/window_gtk.h
@@ -28,7 +28,7 @@ class GTKWindow : public Window {
   ~GTKWindow() override;
 
   NativePlatformHandle native_platform_handle() const override {
-    return nullptr;
+    return connection_;
   }
   NativeWindowHandle native_handle() const override { return window_; }
 
@@ -67,6 +67,7 @@ class GTKWindow : public Window {
  private:
   void Create();
   GtkWidget* window_;
+  xcb_connection_t* connection_;
 
   friend void gtk_event_handler_(GtkWidget*, GdkEvent*, gpointer);
   bool HandleMouse(GdkEventAny* event);
