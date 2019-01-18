10e5425155042c4c305520d563aac035d43e4749
Sandy Carter <bwrsandman@gmail.com>
2019-01-18 09:05:42 -0500

    [vulkan] Fix typos and cast types

--- src/xenia/ui/vulkan/vulkan_context.cc
+++ src/xenia/ui/vulkan/vulkan_context.cc
@@ -38,7 +38,7 @@ VulkanContext::VulkanContext(VulkanProvider* provider, Window* target_window)
 
 VulkanContext::~VulkanContext() {
   VkResult status;
-  auto provider = static_cast<VulkanProvider*>(provider_);
+  auto provider = dynamic_cast<VulkanProvider*>(provider_);
   auto device = provider->device();
   {
     std::lock_guard<std::mutex> queue_lock(device->primary_queue_mutex());
@@ -49,8 +49,7 @@ VulkanContext::~VulkanContext() {
 }
 
 bool VulkanContext::Initialize() {
-  auto provider = static_cast<VulkanProvider*>(provider_);
-  auto device = provider->device();
+  auto provider = dynamic_cast<VulkanProvider*>(provider_);
 
   if (target_window_) {
     // Create swap chain used to present to the window.
--- src/xenia/ui/vulkan/vulkan_instance.cc
+++ src/xenia/ui/vulkan/vulkan_instance.cc
@@ -337,7 +337,7 @@ bool VulkanInstance::CreateInstance() {
       return false;
   }
 
-  // Load Vulkan entrypoints and extensions.
+  // Load Vulkan entry points and extensions.
   volkLoadInstance(handle);
 
   // Enable debug validation, if needed.
