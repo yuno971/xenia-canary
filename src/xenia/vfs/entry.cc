e22cc86067875489ec3adc4814d4e940b6fcc337
Sandy Carter <bwrsandman@gmail.com>
2019-02-03 10:15:15 -0500

    [fs linux] Fix sep of absolute paths of entries

--- src/xenia/vfs/entry.cc
+++ src/xenia/vfs/entry.cc
@@ -27,7 +27,8 @@ Entry::Entry(Device* device, Entry* parent, const std::string& path)
       access_timestamp_(0),
       write_timestamp_(0) {
   assert_not_null(device);
-  absolute_path_ = xe::join_paths(device->mount_path(), path, '\\');
+  absolute_path_ = xe::join_paths(device->mount_path(),
+                                  xe::fix_path_separators(path, '\\'), '\\');
   name_ = xe::find_name_from_path(path);
 }
 
