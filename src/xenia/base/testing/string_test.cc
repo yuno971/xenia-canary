bwrsandman/xenia

[string] Add base string tests

src/xenia/base/testing/string_test.cc
@@ -1,0 +1,25 @@
+/**
+******************************************************************************
+* Xenia : Xbox 360 Emulator Research Project                                 *
+******************************************************************************
+* Copyright 2019 Ben Vanik. All rights reserved.                             *
+* Released under the BSD license - see LICENSE in the root for more details. *
+******************************************************************************
+*/
+
+#include "xenia/base/string.h"
+
+#include "third_party/catch/include/catch.hpp"
+
+namespace xe {
+namespace base {
+namespace test {
+
+TEST_CASE("find_base_path") {
+  // TODO(bwrsandman):
+  REQUIRE(false);
+}
+
+}  // namespace test
+}  // namespace base
+}  // namespace xe

yuno971/xenia-canary

src/xenia/base/testing/string_test.cc
@@ -1,0 +1,25 @@
+/**
+******************************************************************************
+* Xenia : Xbox 360 Emulator Research Project                                 *
+******************************************************************************
+* Copyright 2019 Ben Vanik. All rights reserved.                             *
+* Released under the BSD license - see LICENSE in the root for more details. *
+******************************************************************************
+*/
+
+#include "xenia/base/string.h"
+
+#include "third_party/catch/include/catch.hpp"
+
+namespace xe {
+namespace base {
+namespace test {
+
+TEST_CASE("StringBuffer") {
+  // TODO(bwrsandman):
+  REQUIRE(false);
+}
+
+}  // namespace test
+}  // namespace base
+}  // namespace xe
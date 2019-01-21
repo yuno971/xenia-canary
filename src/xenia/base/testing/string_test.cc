6c58b3b13dbbf8413dc421a84e9187e799dca9e6
Sandy Carter <bwrsandman@gmail.com>
2019-01-20 21:28:17 -0500

    [string] Add base string tests

---
+++ src/xenia/base/testing/string_test.cc
@@ -0,0 +1,25 @@
+/**
+******************************************************************************
+* Xenia : Xbox 360 Emulator Research Project                                 *
+******************************************************************************
+* Copyright 2019 Ben Vanik. All rights reserved.                             *
+* Released under the BSD license - see LICENSE in the root for more details. *
+******************************************************************************
+*/
+
+#include "xenia/base/string.h"
+
+#include "third_party/catch/include/catch.hpp"
+
+namespace xe {
+namespace base {
+namespace test {
+
+TEST_CASE("StringBuffer") {
+  // TODO(bwrsandman):
+  REQUIRE(false);
+}
+
+}  // namespace test
+}  // namespace base
+}  // namespace xe
