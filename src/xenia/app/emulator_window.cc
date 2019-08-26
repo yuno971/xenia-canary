77c23005e2569cf7dcab6339fb84a67905e7bf08
illusion98 <37698908+illusion98@users.noreply.github.com>
2019-08-26 04:32:41 -0500

    Change window title
    xenia -> xenia-canary

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -41,7 +41,7 @@ using xe::ui::MenuItem;
 using xe::ui::MouseEvent;
 using xe::ui::UIEvent;
 
-const std::string kBaseTitle = "Xenia";
+const std::string kBaseTitle = "xenia-canary";
 
 EmulatorWindow::EmulatorWindow(Emulator* emulator,
                                ui::WindowedAppContext& app_context)
