df83a5c83840f7b4d3b37ea6ac1202033361917c
emoose <abc@cock.li>
2020-01-02 23:59:50 +0000

    [HID] Allow winkey driver to use the first unused user index
    This removes the user_index == 0 requirement from the InputSystem code, and updates WinKeyInputDriver to use the first non-connected user index if it can.
    
    Eg. if you had 2 XInput controllers plugged in, those two will take up user index 0 and 1, and keyboard will take user index 2.
    If all four indexes are taken up already, the WinKey driver will be disabled.
    
    (This is done by passing already-setup drivers to each drivers Setup function: since WinKey is the last to be setup, this'll let it query the XInput driver and find which user_index it should handle)

--- src/xenia/hid/input_system.cc
+++ src/xenia/hid/input_system.cc
@@ -30,10 +30,6 @@ X_RESULT InputSystem::GetCapabilities(uint32_t user_index, uint32_t flags,
                                       X_INPUT_CAPABILITIES* out_caps) {
   SCOPE_profile_cpu_f("hid");
 
-  if (user_index && user_index != 0xFF) {
-    return X_ERROR_NO_SUCH_USER;
-  }
-
   bool any_connected = false;
   for (auto& driver : drivers_) {
     X_RESULT result = driver->GetCapabilities(user_index, flags, out_caps);
@@ -49,10 +45,6 @@ X_RESULT InputSystem::GetCapabilities(uint32_t user_index, uint32_t flags,
 
 X_RESULT InputSystem::GetState(uint32_t user_index, X_INPUT_STATE* out_state) {
   SCOPE_profile_cpu_f("hid");
-  
-  if (user_index && user_index != 0xFF) {
-    return X_ERROR_NO_SUCH_USER;
-  }
 
   bool any_connected = false;
   for (auto& driver : drivers_) {
@@ -70,10 +62,6 @@ X_RESULT InputSystem::GetState(uint32_t user_index, X_INPUT_STATE* out_state) {
 X_RESULT InputSystem::SetState(uint32_t user_index,
                                X_INPUT_VIBRATION* vibration) {
   SCOPE_profile_cpu_f("hid");
-  
-  if (user_index && user_index != 0xFF) {
-    return X_ERROR_NO_SUCH_USER;
-  }
 
   bool any_connected = false;
   for (auto& driver : drivers_) {
@@ -91,10 +79,6 @@ X_RESULT InputSystem::SetState(uint32_t user_index,
 X_RESULT InputSystem::GetKeystroke(uint32_t user_index, uint32_t flags,
                                    X_INPUT_KEYSTROKE* out_keystroke) {
   SCOPE_profile_cpu_f("hid");
-  
-  if (user_index && user_index != 0xFF) {
-    return X_ERROR_NO_SUCH_USER;
-  }
 
   bool any_connected = false;
   for (auto& driver : drivers_) {
