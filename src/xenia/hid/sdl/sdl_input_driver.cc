0f5445aa3c876d2ad55082aa7236c8e18388131c
Cancerous <randprint@gmail.com>
2020-02-02 04:11:23 -0500

    [HID] SDL Input PR merge fixes

--- src/xenia/hid/sdl/sdl_input_driver.cc
+++ src/xenia/hid/sdl/sdl_input_driver.cc
@@ -51,7 +51,7 @@ SDLInputDriver::~SDLInputDriver() {
   }
 }
 
-X_STATUS SDLInputDriver::Setup() {
+X_STATUS SDLInputDriver::Setup(std::vector<std::unique_ptr<hid::InputDriver>>& drivers) {
   if (!TestSDLVersion()) {
     return X_STATUS_UNSUCCESSFUL;
   }
@@ -92,6 +92,7 @@ X_STATUS SDLInputDriver::Setup() {
       }
       switch (type) {
         case SDL_CONTROLLERDEVICEADDED:
+        case SDL_JOYDEVICEADDED:
           driver->OnControllerDeviceAdded(event);
           break;
         case SDL_CONTROLLERDEVICEREMOVED:
