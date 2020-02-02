bwrsandman/xenia

[HID/Linux] Implement cross platform controller input using SDL2 library.

premake5.lua
@@ -248,6 +248,7 @@ solution("xenia")
   include("src/xenia/gpu/vulkan")
   include("src/xenia/hid")
   include("src/xenia/hid/nop")
+  include("src/xenia/hid/sdlpad")
   include("src/xenia/kernel")
   include("src/xenia/ui")
   include("src/xenia/ui/spirv")

src/xenia/app/premake5.lua
@@ -35,6 +35,7 @@ project("xenia-app")
     "xenia-gpu-vulkan",
     "xenia-hid",
     "xenia-hid-nop",
+    "xenia-hid-sdlpad",
     "xenia-kernel",
     "xenia-ui",
     "xenia-ui-spirv",
@@ -73,15 +74,22 @@ project("xenia-app")
       "X11-xcb",
       "GL",
       "vulkan",
+      "SDL2",
     })
 
   filter("platforms:Windows")
     links({
+      "delayimp", -- Enable dll delayed loading for msvc
       "xenia-apu-xaudio2",
       "xenia-gpu-d3d12",
       "xenia-hid-winkey",
       "xenia-hid-xinput",
       "xenia-ui-d3d12",
+    })
+
+  filter("platforms:Windows")
+    linkoptions({
+      "/DELAYLOAD:SDL2.dll",  -- SDL is not mandatory since on windows, XInput is the preferred input method
     })
 
   filter("platforms:Windows")

src/xenia/hid/premake5.lua
@@ -26,6 +26,7 @@ project("xenia-hid-demo")
     "xenia-base",
     "xenia-hid",
     "xenia-hid-nop",
+    "xenia-hid-sdlpad",
     "xenia-ui",
     "xenia-ui-vulkan",
   })
@@ -48,6 +49,7 @@ project("xenia-hid-demo")
       "X11-xcb",
       "GL",
       "vulkan",
+      "SDL2",
     })
 
   filter("platforms:Windows")

yuno971/xenia-canary

premake5.lua
@@ -239,6 +239,7 @@ solution("xenia")
   include("src/xenia/gpu/vulkan")
   include("src/xenia/hid")
   include("src/xenia/hid/nop")
+  include("src/xenia/hid/sdl")
   include("src/xenia/kernel")
   include("src/xenia/ui")
   include("src/xenia/ui/spirv")

src/xenia/app/premake5.lua
@@ -34,6 +34,7 @@ project("xenia-app")
     "xenia-gpu-vulkan",
     "xenia-hid",
     "xenia-hid-nop",
+    "xenia-hid-sdl",
     "xenia-kernel",
     "xenia-ui",
     "xenia-ui-spirv",
@@ -71,15 +72,22 @@ project("xenia-app")
       "xcb",
       "X11-xcb",
       "vulkan",
+      "SDL2",
     })
 
   filter("platforms:Windows")
     links({
+      "delayimp", -- Enable dll delayed loading for msvc
       "xenia-apu-xaudio2",
       "xenia-gpu-d3d12",
       "xenia-hid-winkey",
       "xenia-hid-xinput",
       "xenia-ui-d3d12",
+    })
+
+  filter("platforms:Windows")
+    linkoptions({
+      "/DELAYLOAD:SDL2.dll",  -- SDL is not mandatory since on windows, XInput is the preferred input method
     })
 
   filter("platforms:Windows")

src/xenia/hid/premake5.lua
@@ -25,6 +25,7 @@ project("xenia-hid-demo")
     "xenia-base",
     "xenia-hid",
     "xenia-hid-nop",
+    "xenia-hid-sdl",
     "xenia-ui",
     "xenia-ui-vulkan",
   })
@@ -42,6 +43,7 @@ project("xenia-hid-demo")
       "xcb",
       "X11-xcb",
       "vulkan",
+      "SDL2",
     })
 
   filter("platforms:Windows")