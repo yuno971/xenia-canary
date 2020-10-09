d78d33d2e99dbbe7913e2ce29bca52e7579e68c4
Gliniak <Gliniak93@gmail.com>
2020-10-09 21:02:32 +0200

    Initial support for xex patching

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -551,6 +551,12 @@ void EmulatorWindow::UpdateTitle() {
         ")";
   }
 
+  auto patching_system = emulator()->patching_system();
+  if (patching_system) {
+    auto title_patched =
+        patching_system->isAnyPatchApplied() ? " [Patches Applied]" : "";
+    title += title_patched;
+  }
   window_->set_title(title);
 }
 
