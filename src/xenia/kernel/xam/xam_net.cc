00f4c75db3c4d0179afe2613ddd8900c831999dd
Gliniak <Gliniak93@gmail.com>
2021-11-17 13:10:06 +0100

    Changed parameters type for NetDll_select

--- src/xenia/kernel/xam/xam_net.cc
+++ src/xenia/kernel/xam/xam_net.cc
@@ -852,7 +852,7 @@ struct host_set {
   }
 };
 
-int_result_t NetDll_select(int_t caller, int_t nfds,
+int_result_t NetDll_select(dword_t caller, dword_t nfds,
                            pointer_t<x_fd_set> readfds,
                            pointer_t<x_fd_set> writefds,
                            pointer_t<x_fd_set> exceptfds,
